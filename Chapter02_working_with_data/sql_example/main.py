9a0e698af0de635c20636c625adc1a5aabb0fee2
Hitesh1595 <hiteshpratap82@gmail.com>
2025-05-24 23:11:10 +0530

    Connection to database sql/nosql using engine

--- Chapter02_working_with_data/sql_example/main.py
+++ Chapter02_working_with_data/sql_example/main.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from database import SessionLocal, User
 
@@ -18,3 +19,75 @@ app = FastAPI()
 def read_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
+
+
+class UserBody(BaseModel):
+    name: str
+    email: str
+
+
+@app.post("/user")
+def add_new_user(
+    user: UserBody, db: Session = Depends(get_db)
+):
+    new_user = User(name=user.name, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/user")
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = (db.query(User).filter(
+        User.id == user_id
+    ).first()
+    )
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User Not Found"
+        )
+
+
+@app.post("/user/{user_id}")
+async def update_user(
+    user_id: int,
+    user: UserBody,
+    db: Session = Depends(get_db)
+):
+    db_user = db.query(User).filter(
+        User.id == user_id
+    ).first()
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not Found"
+        )
+
+    db_user.name = user.name
+    db_user.email = user.email
+    db.commit()
+
+    db.refresh(db_user)
+    return db_user
+
+
+@app.delete("/user")
+def delete_user(
+    user_id: int, db: Session = Depends(get_db)
+):
+    db_user = (
+        db.query(User)
+        .filter(User.id == user_id)
+        .first()
+    )
+    if db_user is None:
+        raise HTTPException(
+            status_code=404, detail="User not found"
+        )
+    db.delete(db_user)
+    db.commit()
+    return {"detail": "User deleted"}
